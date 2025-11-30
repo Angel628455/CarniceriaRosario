1046c0993561827eaf4999f4dc99e36ffded8d6f
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-11-29 19:03:07 -0500

    screen

--- app/src/main/java/ucne/edu/carniceriarosario/presentation/navigation/Screen.kt
+++ app/src/main/java/ucne/edu/carniceriarosario/presentation/navigation/Screen.kt
@@ -1,5 +1,6 @@
 package ucne.edu.carniceriarosario.presentation.navigation
 
+
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -20,6 +21,7 @@ sealed class Screen {
     @Serializable
     data class Perfil(val usuarioId: Int) : Screen()
 
+    // Usuarios
     @Serializable
     data object UsuariosScreen : Screen()
 
@@ -28,4 +30,60 @@ sealed class Screen {
 
     @Serializable
     data object CrearUsuario : Screen()
+
+    // Carrito
+    @Serializable
+    data object CarritoListScreen : Screen()
+
+    @Serializable
+    data class CreateCarritoScreen(val carritoId: Int? = null) : Screen()
+
+    // Categorías
+    @Serializable
+    data object CategoriaCarnesListScreen : Screen()
+
+    @Serializable
+    data class CreateCategoriaScreen(val categoriaId: Int? = null) : Screen()
+
+    // Clientes
+    @Serializable
+    data object ClienteListScreen : Screen()
+
+    @Serializable
+    data class CreateClienteScreen(val clienteId: Int? = null) : Screen()
+
+    // Estados
+    @Serializable
+    data object EstadosListScreen : Screen()
+
+    @Serializable
+    data class CreateEstadoScreen(val estadoId: Int? = null) : Screen()
+
+    // Métodos de Pago
+    @Serializable
+    data object MetodosPagosListScreen : Screen()
+
+    @Serializable
+    data class CreateMetodoPagoScreen(val metodoPagoId: Int? = null) : Screen()
+
+    // Pagos
+    @Serializable
+    data object PagosListScreen : Screen()
+
+    @Serializable
+    data class CreatePagoScreen(val pagoId: Int? = null) : Screen()
+
+    // Pedidos
+    @Serializable
+    data object PedidosListScreen : Screen()
+
+    @Serializable
+    data class CreatePedidoScreen(val pedidoId: Int? = null) : Screen()
+
+    // Productos
+    @Serializable
+    data object ProductoListScreen : Screen()
+
+    @Serializable
+    data class CreateProductoScreen(val productoId: Int? = null) : Screen()
 }
