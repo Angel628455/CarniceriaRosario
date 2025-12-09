8178fa52259451d3507e21bdd4c2849a67f0b81d
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-12-09 15:42:04 -0500

    Creacion de DataRemote

--- app/src/main/java/ucne/edu/carniceriarosario/data/remote/RemoteDataSource.kt
+++ app/src/main/java/ucne/edu/carniceriarosario/data/remote/RemoteDataSource.kt
@@ -25,84 +25,86 @@ class RemoteDataSource @Inject constructor(
     private val pagosApi: PagosApi,
     private val pedidosApi: PedidosApi,
     private val productosApi: ProductosApi,
-    private val estadisticasApi: EstadisticasApi
-
+    private val estadisticasApi: EstadisticasApi,
+    private val detalleDeCarritoApi: DetalleDeCarritoApi
 ) {
 
-    // ------------------------- USUARIOS -------------------------
     suspend fun getUsuarios() = usuarioApi.getUsuarios()
     suspend fun getUsuario(id: Int) = usuarioApi.getUsuario(id)
     suspend fun createUsuario(data: UsuarioDto) = usuarioApi.createUsuario(data)
     suspend fun updateUsuario(id: Int, data: UsuarioDto) = usuarioApi.updateUsuario(id, data)
     suspend fun deleteUsuario(id: Int) = usuarioApi.deleteUsuario(id)
 
-    // --------------------- ESTADISTICAS ---------------------
+
     suspend fun getEstadisticas() = estadisticasApi.getEstadisticas()
 
-    // ------------------------- CARRITOS -------------------------
+
     suspend fun getCarritos() = carritoApi.getCarritos()
     suspend fun getCarrito(id: Int) = carritoApi.getCarrito(id)
     suspend fun createCarrito(data: CarritoDeComprasDto) = carritoApi.createCarrito(data)
     suspend fun updateCarrito(id: Int, data: CarritoDeComprasDto) = carritoApi.updateCarrito(id, data)
     suspend fun deleteCarrito(id: Int) = carritoApi.deleteCarrito(id)
 
-    // ------------------------- CATEGORIAS -------------------------
+
+    suspend fun getDetallesDeCarrito() = detalleDeCarritoApi.getCarritos()
+
+
     suspend fun getCategorias() = categoriaApi.getCategorias()
     suspend fun getCategoria(id: Int) = categoriaApi.getCategoria(id)
     suspend fun createCategoria(data: CategoriaCarnesDto) = categoriaApi.createCategoria(data)
     suspend fun updateCategoria(id: Int, data: CategoriaCarnesDto) = categoriaApi.updateCategoria(id, data)
     suspend fun deleteCategoria(id: Int) = categoriaApi.deleteCategoria(id)
 
-    // ------------------------- CLIENTES -------------------------
+
     suspend fun getClientes() = clienteApi.getClientes()
     suspend fun getCliente(id: Int) = clienteApi.getCliente(id)
     suspend fun createCliente(data: ClienteDto) = clienteApi.createCliente(data)
     suspend fun updateCliente(id: Int, data: ClienteDto) = clienteApi.updateCliente(id, data)
     suspend fun deleteCliente(id: Int) = clienteApi.deleteCliente(id)
 
-    // ------------------------- DETALLE PRODUCTOS -------------------------
+
     suspend fun getDetallesProductos() = detalleProductosApi.getDetalles()
     suspend fun getDetalleProducto(id: Int) = detalleProductosApi.getDetalle(id)
     suspend fun createDetalleProducto(data: DetalleProductosDto) = detalleProductosApi.createDetalle(data)
     suspend fun updateDetalleProducto(id: Int, data: DetalleProductosDto) = detalleProductosApi.updateDetalle(id, data)
     suspend fun deleteDetalleProducto(id: Int) = detalleProductosApi.deleteDetalle(id)
 
-    // ------------------------- DETALLE PAGOS -------------------------
+
     suspend fun getDetallesPagos() = detallePagosApi.getDetallesPagos()
     suspend fun getDetallePago(id: Int) = detallePagosApi.getDetallePago(id)
     suspend fun createDetallePago(data: DetallesPagosDto) = detallePagosApi.createDetallePago(data)
     suspend fun updateDetallePago(id: Int, data: DetallesPagosDto) = detallePagosApi.updateDetallePago(id, data)
     suspend fun deleteDetallePago(id: Int) = detallePagosApi.deleteDetallePago(id)
 
-    // ------------------------- ESTADOS -------------------------
+
     suspend fun getEstados() = estadosApi.getEstados()
     suspend fun getEstado(id: Int) = estadosApi.getEstado(id)
     suspend fun createEstado(data: EstadosDto) = estadosApi.createEstado(data)
     suspend fun updateEstado(id: Int, data: EstadosDto) = estadosApi.updateEstado(id, data)
     suspend fun deleteEstado(id: Int) = estadosApi.deleteEstado(id)
 
-    // ------------------------- METODOS DE PAGO -------------------------
+
     suspend fun getMetodosPago() = metodosPagosApi.getMetodos()
     suspend fun getMetodoPago(id: Int) = metodosPagosApi.getMetodo(id)
     suspend fun createMetodoPago(data: MetodosPagosDto) = metodosPagosApi.createMetodo(data)
     suspend fun updateMetodoPago(id: Int, data: MetodosPagosDto) = metodosPagosApi.updateMetodo(id, data)
     suspend fun deleteMetodoPago(id: Int) = metodosPagosApi.deleteMetodo(id)
 
-    // ------------------------- PAGOS -------------------------
+
     suspend fun getPagos() = pagosApi.getPagos()
     suspend fun getPago(id: Int) = pagosApi.getPago(id)
     suspend fun createPago(data: PagosDto) = pagosApi.createPago(data)
     suspend fun updatePago(id: Int, data: PagosDto) = pagosApi.updatePago(id, data)
     suspend fun deletePago(id: Int) = pagosApi.deletePago(id)
 
-    // ------------------------- PEDIDOS -------------------------
+
     suspend fun getPedidos() = pedidosApi.getPedidos()
     suspend fun getPedido(id: Int) = pedidosApi.getPedido(id)
     suspend fun createPedido(data: PedidosDto) = pedidosApi.createPedido(data)
     suspend fun updatePedido(id: Int, data: PedidosDto) = pedidosApi.updatePedido(id, data)
     suspend fun deletePedido(id: Int) = pedidosApi.deletePedido(id)
 
-    // ------------------------- PRODUCTOS -------------------------
+
     suspend fun getProductos() = productosApi.getProductos()
     suspend fun getProducto(id: Int) = productosApi.getProducto(id)
     suspend fun createProducto(data: ProductosDto) = productosApi.createProducto(data)
