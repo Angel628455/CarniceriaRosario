bb71c22c0f58265e45cf94698d09eac474c48a99
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-12-09 15:25:40 -0500

    Creacion de Api de Categoria de carnes

--- app/src/main/java/ucne/edu/carniceriarosario/data/remote/CategoriaCarnesApi.kt
+++ app/src/main/java/ucne/edu/carniceriarosario/data/remote/CategoriaCarnesApi.kt
@@ -5,7 +5,7 @@ import ucne.edu.carniceriarosario.data.remote.dto.CategoriaCarnesDto
 
 interface CategoriaCarnesApi {
 
-    @GET("api/CategoriasCarnes")
+    @GET("api/CategoriaCarnes")
     suspend fun getCategorias(): List<CategoriaCarnesDto>
 
     @GET("api/CategoriasCarnes/{id}")
