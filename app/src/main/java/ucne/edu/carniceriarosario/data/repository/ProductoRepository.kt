941dce6138b2092a365023bf76837ed84842225d
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-12-09 15:40:24 -0500

    Creacion de Repositorio de producto

--- app/src/main/java/ucne/edu/carniceriarosario/data/repository/ProductoRepository.kt
+++ app/src/main/java/ucne/edu/carniceriarosario/data/repository/ProductoRepository.kt
@@ -14,7 +14,9 @@ class ProductoRepository @Inject constructor(
     fun getProductos(): Flow<Resource<List<ProductosDto>>> = flow {
         emit(Resource.Loading())
         try {
-            emit(Resource.Success(remoteDataSource.getProductos()))
+            val result = remoteDataSource.getProductos()
+            emit(Resource.Success(result))
+            //emit(Resource.Success(remoteDataSource.getProductos()))
         } catch (e: Exception) {
             emit(Resource.Error(e.message ?: "Error al obtener productos"))
         }
